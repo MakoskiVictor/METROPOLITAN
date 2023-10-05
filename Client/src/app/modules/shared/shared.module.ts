68e8af6dcf883c0ff6ecdc8e92d723b3dda95941
MakoskiVictor <makoski.ed@gmail.com>
2023-10-05 10:10:54 -0300

    update import

--- Client/src/app/modules/shared/shared.module.ts
+++ Client/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnimatedButtonComponent } from './components';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AnimatedButtonComponent
+  ],
   imports: [
     CommonModule
   ]
