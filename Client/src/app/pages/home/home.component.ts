ae0851b5db45b8fe539d7887fa3f48c706dc32c3
MakoskiVictor <makoski.ed@gmail.com>
2023-10-06 19:17:50 -0300

    add @HostListener

--- Client/src/app/pages/home/home.component.ts
+++ Client/src/app/pages/home/home.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   showImage = false;
+  showExperienceSectionAnimation = false;
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+
+    // Obtener la posición del componente en la página
+    const componentPosition = this.el.nativeElement.offsetTop;
+
+    // Obtener la posición actual de desplazamiento en la ventana
+    const scrollPosition = window.pageYOffset;
+
+    // Verificar si el componente está a la vista (en la mitad de la ventana)
+    if (scrollPosition >= componentPosition - window.innerHeight) {
+      this.showExperienceSectionAnimation = !this.showExperienceSectionAnimation;
+    } else {
+      this.showExperienceSectionAnimation = false;
+    }
+  }
 
   showAnimation() {
     this.showImage = true;
