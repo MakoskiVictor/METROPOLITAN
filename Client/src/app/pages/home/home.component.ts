596ab6b4ebcfcf7c991fced6ff08cb54a9078c07
MakoskiVictor <makoski.ed@gmail.com>
2023-10-06 09:18:03 -0300

    add showAnimation fn

--- Client/src/app/pages/home/home.component.ts
+++ Client/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  showImage = false;
+
+  showAnimation() {
+    this.showImage = true;
+  }
 
 }
