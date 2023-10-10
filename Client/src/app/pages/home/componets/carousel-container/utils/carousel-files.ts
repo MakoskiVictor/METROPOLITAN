66cf05b3dcbea55c1abcdb60c1fd1c41307049b5
MakoskiVictor <makoski.ed@gmail.com>
2023-10-10 13:11:00 -0300

    move carousel files

---
+++ Client/src/app/pages/home/componets/carousel-container/utils/carousel-files.ts
@@ -0,0 +1,34 @@
+export const carouselFiles = [
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-01.jpg',
+    title: 'Espacio-1'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-02.jpg',
+    title: 'Espacio-2'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-03.jpg',
+    title: 'Espacio-3'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-04.jpg',
+    title: 'Espacio-4'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-05.jpg',
+    title: 'Espacio-5'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-06.jpg',
+    title: 'Espacio-6'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-07.jpg',
+    title: 'Espacio-7'
+  },
+  {
+    src: 'assets/carousel/metrpolitan-slider-espacios-08.jpg',
+    title: 'Espacio-8'
+  },
+]
