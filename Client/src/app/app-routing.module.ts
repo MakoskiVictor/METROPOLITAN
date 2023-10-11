c21541c776248c42441397a7dbff6a62c0bdacdd
MakoskiVictor <makoski.ed@gmail.com>
2023-10-11 20:24:54 -0300

    add route to working module

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages';
+import { HomeComponent, WorkingComponent } from './pages';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'working', component: WorkingComponent }
 ];
 
 @NgModule({
