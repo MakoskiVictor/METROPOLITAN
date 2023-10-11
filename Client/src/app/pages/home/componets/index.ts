4b369815366228fff29c829bd0d69d1e4f4f5ba4
MakoskiVictor <makoski.ed@gmail.com>
2023-10-11 10:30:04 -0300

    update barrels

--- Client/src/app/pages/home/componets/index.ts
+++ Client/src/app/pages/home/componets/index.ts
@@ -2,3 +2,4 @@ export * from './carousel-container';
 export * from './carousel';
 export * from './carousel/models';
 export * from './carousel-container/utils';
+export * from './fitness-section';
