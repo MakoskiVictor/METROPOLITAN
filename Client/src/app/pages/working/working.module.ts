52243ee4e44ddf71b7e25fa46fd3b91456971fd1
MakoskiVictor <makoski.ed@gmail.com>
2023-10-11 20:23:38 -0300

    create working module

---
+++ Client/src/app/pages/working/working.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkingComponent } from './working.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    WorkingComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    WorkingComponent
+  ]
+})
+export class WorkingModule { }
