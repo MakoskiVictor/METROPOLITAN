7814999e19912b8823e044af51087172c64203f4
MakoskiVictor <makoski.ed@gmail.com>
2023-10-18 10:10:01 -0300

    update imports

--- Client/src/app/pages/login/login.module.ts
+++ Client/src/app/pages/login/login.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { LoginFormComponent } from './components';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LoginFormComponent
+  ],
   imports: [
     CommonModule
   ]
