2e36df98dad5fedcf126add82c59210a663fa41d
MakoskiVictor <makoski.ed@gmail.com>
2023-10-18 11:45:09 -0300

    create legal information module

---
+++ Client/src/app/pages/legal-information/legal-information.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LegalInformationComponent } from './legal-information.component';
+
+
+
+@NgModule({
+  declarations: [
+    LegalInformationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LegalInformationModule { }
