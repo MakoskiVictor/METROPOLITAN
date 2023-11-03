1d9617dd00f6976611c602c707121fc0284df848
MakoskiVictor <makoski.ed@gmail.com>
2023-11-03 08:47:24 -0300

    set detection strategy to OnPush

--- Client/src/app/pages/recover-password/recover-password.component.ts
+++ Client/src/app/pages/recover-password/recover-password.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
-  styleUrls: ['./recover-password.component.scss']
+  styleUrls: ['./recover-password.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecoverPasswordComponent {
 
