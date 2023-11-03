84bc259d332dd86e189b70fa26b9c0bbee1d5a40
MakoskiVictor <makoski.ed@gmail.com>
2023-11-03 08:47:17 -0300

    set detection strategy to OnPush

--- Client/src/app/pages/home/componets/fitness-section/fitness-section.component.ts
+++ Client/src/app/pages/home/componets/fitness-section/fitness-section.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-fitness-section',
   templateUrl: './fitness-section.component.html',
-  styleUrls: ['./fitness-section.component.scss']
+  styleUrls: ['./fitness-section.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FitnessSectionComponent {
 
