149c0f08f61c7e2d2df19f020855d01dff056611
MakoskiVictor <makoski.ed@gmail.com>
2023-11-03 08:46:57 -0300

    set detection strategy to OnPush

--- Client/src/app/pages/legal-information/legal-information.component.ts
+++ Client/src/app/pages/legal-information/legal-information.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-legal-information',
   templateUrl: './legal-information.component.html',
-  styleUrls: ['./legal-information.component.scss']
+  styleUrls: ['./legal-information.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LegalInformationComponent {
 
