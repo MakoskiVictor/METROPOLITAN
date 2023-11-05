fa56b44647356597699eb6c5fb81559dddb92f2b
MakoskiVictor <makoski.ed@gmail.com>
2023-11-05 14:19:08 -0300

    create Forms

--- Client/src/app/pages/register/components/register-form/register-form.component.ts
+++ Client/src/app/pages/register/components/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent {
+
+  registerForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required/* , Validators.minLength(5) */]),
+    lastname: new FormControl('', [Validators.required/* , Validators.minLength(5) */]),
+    email: new FormControl('', [Validators.required/* , Validators.minLength(5), Validators.email */]),
+    password: new FormControl('', [Validators.required, /* Validators.minLength(5) *//* , Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$") */])
+  })
+
+  passwordCheck = new FormControl('', [Validators.required]);
+
+  onSubmit () {
+    console.warn(this.registerForm.value);
+  }
 
 }
