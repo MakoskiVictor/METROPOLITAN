8c18c3634cc1adae91ea695e7da97b6867cf93b6
MakoskiVictor <makoski.ed@gmail.com>
2023-11-05 08:32:36 -0300

    import shared module

--- Client/src/app/pages/register/register.module.ts
+++ Client/src/app/pages/register/register.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { SharedModule } from 'src/app/modules';
 
 
 
@@ -12,7 +13,8 @@ import { RegisterFormComponent } from './components/register-form/register-form.
     RegisterFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class RegisterModule { }
