65c55efed1b91b9e7406d96301defd02d1212865
MakoskiVictor <makoski.ed@gmail.com>
2023-11-07 12:18:16 -0300

    add regex to lastname and firstname

--- Client/src/app/pages/register/components/register-form/register-form.component.ts
+++ Client/src/app/pages/register/components/register-form/register-form.component.ts
@@ -18,17 +18,22 @@ export class RegisterFormComponent {
     this.registerForm = new FormGroup({
       firstname: new FormControl('', [
         Validators.required, 
-        Validators.minLength(5)
+        Validators.minLength(5),
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.pattern(/^[^\d]+$/)
       ]),
       lastname: new FormControl('', [
         Validators.required, 
-        Validators.minLength(5)
+        Validators.minLength(5),
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.pattern(/^[^\d]+$/)
       ]),
       email: new FormControl('', [
         Validators.required, 
         Validators.email,
         Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
       ]),
+      checkBox: new FormControl(false, Validators.requiredTrue),
       password: new FormControl('', [
         Validators.required, 
         Validators.minLength(5), 
@@ -47,19 +52,18 @@ export class RegisterFormComponent {
     console.warn(this.registerForm.get('password')?.errors);
   }
 
-  passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors |
-null => {
-const password = control.get('password')?.value;
-const passwordCheck = control.get('passwordCheck')?.value;
-// Solo devuelve un error si ambos campos han sido tocados o están dirty.
-if (control.get('password')?.dirty && control.get('passwordCheck')?.dirty &&
-password !== passwordCheck) {
-// Se recomienda establecer el error en el control específico
-control.get('passwordCheck')?.setErrors({ passwordMismatch: true });
-return { passwordMismatch: true };
-}
-return null;
-};
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const passwordCheck = control.get('passwordCheck')?.value;
+    // Solo devuelve un error si ambos campos han sido tocados o están dirty.
+    if (control.get('password')?.dirty && control.get('passwordCheck')?.dirty &&
+    password !== passwordCheck) {
+    // Se recomienda establecer el error en el control específico
+    control.get('passwordCheck')?.setErrors({ passwordMismatch: true });
+    return { passwordMismatch: true };
+    }
+    return null;
+  }
 
 
 }
