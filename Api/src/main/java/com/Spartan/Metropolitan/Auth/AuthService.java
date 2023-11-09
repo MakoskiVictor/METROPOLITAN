a9441a741e2644c5cdaaee2ed4e01849496f13f5
arroyos <arroyosagustin@gmail.com>
2023-11-09 11:59:00 -0300

    the login was implemented

--- Api/src/main/java/com/Spartan/Metropolitan/Auth/AuthService.java
+++ Api/src/main/java/com/Spartan/Metropolitan/Auth/AuthService.java
@@ -6,6 +6,9 @@ import com.Spartan.Metropolitan.User.User;
 import com.Spartan.Metropolitan.User.UserRepository;
 import com.Spartan.Metropolitan.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +20,21 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
-    
+    private final AuthenticationManager authenticationManager;
     
     public AuthResponse login(LoginRequest request) {
-    return null;
+    
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+    
+        UserDetails user= userRepository.findByEmail(request.getEmail()).orElseThrow();
+        
+        String token = jwtService.getToken(user);
+        
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+        
+        
     }
     
     public AuthResponse register(RegisterRequest request) throws Exception{
