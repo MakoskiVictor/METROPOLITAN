085a7e1a7f605c24a68e254aa9234bfa46fa02ec
arroyos <arroyosagustin@gmail.com>
2023-11-09 18:00:35 -0300

    Fix Secure routes with jwt

--- Api/src/main/java/com/Spartan/Metropolitan/jwt/JwtAuthenticationFilter.java
+++ Api/src/main/java/com/Spartan/Metropolitan/jwt/JwtAuthenticationFilter.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.Spartan.Metropolitan.jwt;
 
+package com.Spartan.Metropolitan.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,17 +22,43 @@ import org.springframework.web.filter.OncePerRequestFilter;
  * @author arroyos
  */
 @Component  
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    
+    
+            
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         
         final String token = getTokenFromRequest(request);
+        final String username; 
+        
         if(token == null) {
         
             filterChain.doFilter(request,response);
             return;
         }
+        
+        username = jwtService.getEmailFromToken(token);
+        
+        if(username != null && SecurityContextHolder.getContext().getAuthentication()== null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            
+            if(jwtService.isTokenValid(token, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+            
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            
+            }
+            
+            
+            
+        }
+        
         filterChain.doFilter(request, response);
     }
 
