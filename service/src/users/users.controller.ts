6270bbacce100d98611353d6025304a9622a58eb
MakoskiVictor <makoski.ed@gmail.com>
2024-01-16 10:42:42 -0300

    comment controllers

--- service/src/users/users.controller.ts
+++ service/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+/* import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -31,4 +31,4 @@ export class UsersController {
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
-}
+} */
