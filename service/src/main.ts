56fb59093f01810127b36de8ba222a370d59c6d2
MakoskiVictor <makoski.ed@gmail.com>
2024-01-16 13:42:08 -0300

    add validator config

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  // Si no pasa las validaciones, arroja error (instalar class-transformer)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
