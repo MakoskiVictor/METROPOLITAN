d4ab1e093a9089ed46c3c4a0f5df8d283ff369dd
MakoskiVictor <makoski.ed@gmail.com>
2024-01-16 13:43:30 -0300

    create login and register services

--- service/src/auth/auth.service.ts
+++ service/src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // Este service debe conectarse con otro módulo para usar el otro servicio: UserService
+  constructor(private readonly usersService: UsersService) {}
+  login() {
+    return 'login';
+  }
+
+  register() {
+    return 'register';
+  }
+}
