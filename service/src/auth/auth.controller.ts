b65e352b1912e2bc419f982d929a5c1dd142d030
MakoskiVictor <makoski.ed@gmail.com>
2024-01-16 13:42:55 -0300

    add post and register

--- service/src/auth/auth.controller.ts
+++ service/src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // Inyección de servicios para poder utilizarlo en el controller
+  constructor(private readonly authService: AuthService) {}
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    console.log(registerDto);
+    return this.authService.register();
+  }
+  @Post('login')
+  login() {
+    return this.authService.login();
+  }
+}
