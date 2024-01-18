33aacd015760fefddaef8b9b48a2c934447004ca
MakoskiVictor <makoski.ed@gmail.com>
2024-01-18 12:41:47 -0300

    config jwtConstants

--- service/src/auth/auth.module.ts
+++ service/src/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/constants/jwtConstants';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: process.env.JWT_SECRET_KEY,
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '1d' },
     }),
   ],
