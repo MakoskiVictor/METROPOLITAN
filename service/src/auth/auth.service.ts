24f24f7e745d6d03fa25540803d4460e70c46dba
MakoskiVictor <makoski.ed@gmail.com>
2024-01-18 10:45:34 -0300

    add register fn with http exception

--- service/src/auth/auth.service.ts
+++ service/src/auth/auth.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
   // Este service debe conectarse con otro módulo para usar el otro servicio: UserService
   constructor(private readonly usersService: UsersService) {}
-  login() {
-    return 'login';
+
+  async register(registerDto: RegisterDto) {
+    const findUserMail = await this.usersService.findOneByEmail(
+      registerDto.email,
+    );
+
+    if (findUserMail) {
+      return new HttpException('Email alredy exists', HttpStatus.CONFLICT);
+    }
+    return await this.usersService.create(registerDto);
   }
 
-  register() {
-    return 'register';
+  login() {
+    return 'login';
   }
 }
