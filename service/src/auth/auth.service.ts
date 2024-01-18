ed91cf810de97ea6edfb05a292ba739641eb4aa8
MakoskiVictor <makoski.ed@gmail.com>
2024-01-18 10:59:18 -0300

    add hashed password in register fn

--- service/src/auth/auth.service.ts
+++ service/src/auth/auth.service.ts
@@ -1,21 +1,25 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   // Este service debe conectarse con otro módulo para usar el otro servicio: UserService
   constructor(private readonly usersService: UsersService) {}
 
-  async register(registerDto: RegisterDto) {
-    const findUserMail = await this.usersService.findOneByEmail(
-      registerDto.email,
-    );
+  async register({ firstname, lastname, email, password }: RegisterDto) {
+    const findUserMail = await this.usersService.findOneByEmail(email);
 
     if (findUserMail) {
       return new HttpException('Email alredy exists', HttpStatus.CONFLICT);
     }
-    return await this.usersService.create(registerDto);
+    return await this.usersService.create({
+      firstname,
+      lastname,
+      email,
+      password: await bcryptjs.hash(password, 10),
+    });
   }
 
   login() {
