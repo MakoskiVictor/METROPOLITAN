b75bcbe076fef0ce669fc0e227a40efc490e2120
MakoskiVictor <makoski.ed@gmail.com>
2024-03-11 09:32:20 -0300

    change img format

--- Client/src/app/pages/home/componets/carousel-container/utils/carousel-files.ts
+++ Client/src/app/pages/home/componets/carousel-container/utils/carousel-files.ts
@@ -1,34 +1,34 @@
 export const carouselFiles = [
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-01.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-01.webp',
     title: 'Espacio-1'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-02.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-02.webp',
     title: 'Espacio-2'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-03.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-03.webp',
     title: 'Espacio-3'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-04.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-04.webp',
     title: 'Espacio-4'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-05.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-05.webp',
     title: 'Espacio-5'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-06.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-06.webp',
     title: 'Espacio-6'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-07.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-07.webp',
     title: 'Espacio-7'
   },
   {
-    src: 'assets/carousel/metrpolitan-slider-espacios-08.jpg',
+    src: 'assets/carousel/metrpolitan-slider-espacios-08.webp',
     title: 'Espacio-8'
   },
 ]
