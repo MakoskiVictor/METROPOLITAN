86689a00b643d64f27b23bb427869b00fb45e116
MakoskiVictor <makoski.ed@gmail.com>
2024-03-13 13:03:08 -0300

    style home

--- register/src/app/page.tsx
+++ register/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home</div>
+    <main className=' h-[calc(100vh-7rem)] flex justify-center items-center '>
+      <div className=" text-slate-500 text-8xl " >Home</div>
     </main>
   );
 }
