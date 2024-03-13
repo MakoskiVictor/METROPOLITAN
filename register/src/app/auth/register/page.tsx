763e9bf253ce5afad1a474188761715845e4ff1d
MakoskiVictor <makoski.ed@gmail.com>
2024-03-13 09:23:07 -0300

    initiate new pages

---
+++ register/src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+export default function RegisterPage () {
+    return(
+        <section>
+            <form action="">
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </form>
+        </section>
+    )
+}
