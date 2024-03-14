c477afc36d1cd0a3fa4e7ecd0249867387e773a6
MakoskiVictor <makoski.ed@gmail.com>
2024-03-14 09:58:17 -0300

    add descriptions

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+type Hello = {
+  Description: string
+  Url: string
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Wellcome to Metropolitan BackEnd!';
+  getHello(): Hello[] {
+    return [
+      {
+        Description: "To make a register",
+        Url: "/auth/register"
+      },
+      {
+        Description: "To make a login",
+        Url: "/auth/login"
+      }
+  ]
   }
 }
