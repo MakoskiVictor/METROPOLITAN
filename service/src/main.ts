ada46ad67a34da8bf450895ab5da781d7947042a
MakoskiVictor <makoski.ed@gmail.com>
2024-03-18 17:32:53 -0300

    set open cors

--- service/src/main.ts
+++ service/src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-  app.enableCors()
-  // app.enableCors({
-  //   origin: 'http://localhost:3001/',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: true,
-  //   optionsSuccessStatus: 200,
-  // });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+  });
 
   // Si no pasa las validaciones, arroja error (instalar class-transformer)
   app.useGlobalPipes(
