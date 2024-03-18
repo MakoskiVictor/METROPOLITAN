833b99756523db1d88e98d85ff47df6b69be7bd4
MakoskiVictor <makoski.ed@gmail.com>
2024-03-18 17:33:02 -0300

    add description

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@nestjs/common';
 type Hello = {
   Description: string
+  Method: string
   Url: string
+  Body: BodiContent
 }
+type BodiContent = {
+  firstname?: string
+  lastname?: string
+  email: string
+  password: string
+}
+
 @Injectable()
 export class AppService {
   getHello(): Hello[] {
     return [
       {
         Description: "To make a register",
-        Url: "/auth/register"
+        Method: "POST",
+        Url: "/auth/register",
+        Body: {
+          firstname: "string",
+          lastname: "string",
+          email: "string",
+          password: "string"
+        }
       },
       {
         Description: "To make a login",
-        Url: "/auth/login"
+        Method: "POST",
+        Url: "/auth/login",
+        Body: {
+          email: "string",
+          password: "string"
+        }
       }
   ]
   }
