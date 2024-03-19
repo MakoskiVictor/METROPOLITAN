675b252385aca6a9fd8e3014a916cd1d2e84e7f9
MakoskiVictor <makoski.ed@gmail.com>
2024-03-19 11:36:36 -0300

    erase comments

--- service/src/users/users.service.ts
+++ service/src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-/* import { UpdateUserDto } from './dto/update-user.dto'; */
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -19,20 +18,4 @@ export class UsersService {
   findOneByEmail(email: string) {
     return this.userRepository.findOneBy({ email });
   }
-
-  /* findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  } */
 }
